ac04287a636446804d864628b96958ecc4d74bce
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-02-01 16:01:59 +0900

    Refactor: write form refactor
    lombok 추가
    
    Resolves: #2
    Related to: #1

--- src/main/java/com/example/miniproject/entity/Article.java
+++ src/main/java/com/example/miniproject/entity/Article.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class Article {
     @Id //대푯값
     @GeneratedValue
@@ -16,17 +20,8 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content)
-    {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    @Override
-    public String toString()
-    {
-        return "Article: title = " + title + ", content  = "+ content + id;
-    }
+    public Article() {
 
+    }
 }
