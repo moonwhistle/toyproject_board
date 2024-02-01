fa57dd1ca9c5ad07262fa1b13a6f51a24f846d0a
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-02-01 16:31:39 +0900

    Feat: 게시물 Read 기능 구현
    개별 데이터 조회 API 개발
    
    Related to: #5

--- src/main/java/com/example/miniproject/controller/ArticleController.java
+++ src/main/java/com/example/miniproject/controller/ArticleController.java
@@ -6,8 +6,12 @@ import com.example.miniproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j //로깅 어노테이션 추가
@@ -33,5 +37,18 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model)
+    {
+        log.info("id = "+id); //id 잘 받았는지 확인
+
+        //1. id 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 모델에 데잍처 등록하기
+        model.addAttribute("article", articleEntity);
+        //3 뷰 페이지 반환
+        return "articles/show";
     }
 }
--- src/main/java/com/example/miniproject/repository/ArticleRepository.java
+++ src/main/java/com/example/miniproject/repository/ArticleRepository.java
@@ -3,5 +3,7 @@ package com.example.miniproject.repository;
 import com.example.miniproject.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ArticleRepository extends CrudRepository <Article, Long>{
+
+
+public interface ArticleRepository extends CrudRepository <Article, Long> {
 }
