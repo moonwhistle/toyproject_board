925f1cf4db5ed80fe07e91c434353068873416b0
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-02-14 20:07:07 +0900

    Feat: 댓글 엔티티, 댓글 레파지토리 추가
    레파지토리 테스트 코드 작성, orm.xml
    
    Resolves: #18

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -1,3 +1,19 @@
 insert into article(title,content) values ('가가가','111');
 insert into article(title,content) values ('나나나','222');
 insert into article(title,content) values ('다다다','333');
+
+insert into article(title,content) values ('당신의 인생 영화','댓글 고');
+insert into article(title,content) values ('당신의 소울푸드','댓글 고고');
+insert into article(title,content) values ('당신의 취미','댓글 고고고');
+
+insert into comment(article_id, nickname, body) values (4,'park','굿 윌 헌팅');
+insert into comment(article_id, nickname, body) values (4,'kim','아이언맨');
+insert into comment(article_id, nickname, body) values (4,'choi','스파이더맨');
+
+insert into comment(article_id, nickname, body) values (5,'park','치킨');
+insert into comment(article_id, nickname, body) values (5,'kim','짜장면');
+insert into comment(article_id, nickname, body) values (5,'choi','짬뽕');
+
+insert into comment(article_id, nickname, body) values (6,'park','조깅');
+insert into comment(article_id, nickname, body) values (6,'kim','독서');
+insert into comment(article_id, nickname, body) values (6,'choi','배드민턴');
