84f23eb501c20e11fe5a4cb48f1f10d4809bc94e
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-02-23 16:43:52 +0900

    Refactor: 댓글 서비스 조회 메서드 코드 수정
    스트림 메소드 사용하여 코드 더 간결하게 refactor
    
    Related to : #22

--- src/main/java/com/example/miniproject/service/CommentService.java
+++ src/main/java/com/example/miniproject/service/CommentService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CommentService {
@@ -19,7 +20,7 @@ public class CommentService {
 
     public List<CommentDto> comments(Long articleId)
     {
-        //1. 댓글 조회
+        /*//1. 댓글 조회
         List<Comment> comments = commentRepository.findByArticleId(articleId);
         //2. 엔티티 -> DTO 변환
         List<CommentDto> dtos = new ArrayList<CommentDto>();
@@ -30,6 +31,11 @@ public class CommentService {
             dtos.add(dto);
         }
         //3. 결과 반환
-        return dtos;
+        return dtos;*/
+
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment)) //.map(a->b) : 스트림의 각 요소(a)를 꺼내 b를 수행한 결과로 매핑
+                .collect(Collectors.toList()); //스트림을 리스트로 변환
     }
 }
