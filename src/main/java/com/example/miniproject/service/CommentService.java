54cdf7f8fc5f0370347ddc397687f4355edcb177
Orange flavored banana <106858113+moonwhistle@users.noreply.github.com>
2024-02-23 16:37:58 +0900

    Feat: 댓글 컨트롤러 메소드, 댓글 서비스 메소드 추가, 댓글 DTO 생성
    컨트롤러 : 댓글 조회하는 메소드 추가
    서비스 : 댓글 조회 메소드 추가
    DTO : 댓글 엔티티 -> DTO로 반환받는 메소드 추가
    Related to : #22

--- src/main/java/com/example/miniproject/service/CommentService.java
+++ src/main/java/com/example/miniproject/service/CommentService.java
@@ -1,9 +1,14 @@
 package com.example.miniproject.service;
 
+import com.example.miniproject.dto.CommentDto;
+import com.example.miniproject.entity.Comment;
 import com.example.miniproject.repository.ArticleRepository;
 import com.example.miniproject.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CommentService {
@@ -11,4 +16,20 @@ public class CommentService {
     private CommentRepository commentRepository;
     @Autowired
     private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId)
+    {
+        //1. 댓글 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        //2. 엔티티 -> DTO 변환
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for(int i=0 ;i<comments.size();i++)
+        {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }
+        //3. 결과 반환
+        return dtos;
+    }
 }
